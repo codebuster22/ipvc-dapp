e38441e61afddb1761f13e291cbe593942db3250
Angel Lakra <angel@ivypods.com>
2021-07-27 22:22:43 +0530

    Fixed pdfjs-dist version to 2.8.335

--- src/utils/parsing.ts
+++ src/utils/parsing.ts
@@ -2,7 +2,7 @@
 /* eslint-disable @typescript-eslint/ban-ts-comment */
 import * as pdfjsLib from 'pdfjs-dist';
 import * as pdfjsWorker from 'pdfjs-dist/build/pdf.worker.entry';
-import { PDFDocumentProxy, TextItem, TextMarkedContent } from 'pdfjs-dist/types/display/api';
+import { PDFDocumentProxy, TextItem } from 'pdfjs-dist/types/display/api';
 import Tesseract from 'tesseract.js';
 
 pdfjsLib.GlobalWorkerOptions.workerSrc = pdfjsWorker.workerSrc;
