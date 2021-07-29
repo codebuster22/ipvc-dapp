ba3cc0babafa6dcbf4075f6c37f22ecbd31aef9e
Angel Lakra <angel@ivypods.com>
2021-07-29 13:25:19 +0530

    Added Login Page UI and features

---
+++ pages/login/index.tsx
@@ -0,0 +1,27 @@
+import React from 'react';
+import Head from 'next/head';
+import LoginComp from '@/containers/Login';
+
+const LoginPage = (): JSX.Element => {
+	return (
+		<React.Fragment>
+			<Head>
+				<title>Login</title>
+			</Head>
+			<LoginComp />
+		</React.Fragment>
+	);
+};
+
+export default LoginPage;
+
+LoginPage.getInitialProps = async (ctx): Promise<any> => {
+	if (ctx?.req?.cookies['access_token']) {
+		ctx.res.writeHead(301, {
+			Location: '/onboarding',
+		});
+		ctx.res.end();
+	} else {
+		return {};
+	}
+};
---
+++ pages/onboarding/index.tsx
@@ -0,0 +1,8 @@
+import OnboardingComp from '@/containers/Onboarding';
+import React from 'react';
+
+const OnboardingPage = (): JSX.Element => {
+	return <OnboardingComp />;
+};
+
+export default OnboardingPage;
