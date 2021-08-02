90922fd16648638823a1b63a51948510e968aa22
Mihirsinh Parmar <mihirsinh.parmar.it@gmail.com>
2021-08-02 09:58:50 +0530

    added try catch block to useContract in case contract aren't deployed on this network

--- src/ethereum/useContract.js
+++ src/ethereum/useContract.js
@@ -1,24 +1,29 @@
-import { ethers } from "ethers";
-import { useEffect, useState } from "react";
+import { ethers } from 'ethers';
+import { useEffect, useState } from 'react';
 import contracts from '../contracts/contracts.json';
 
 const useContract = (contractName, provider) => {
-    const [contract, setContract] = useState();
+	const [contract, setContract] = useState();
 
-    const getContractDetails = (contractName, provider) => {
-        const network = contracts[provider.provider.networkVersion];
-        const contractDetails = network[Object.keys(network)[0]].contracts[contractName];
-        return {address: contractDetails.address, abi: contractDetails.abi};
-    }
+	const getContractDetails = (contractName, provider) => {
+		const network = contracts[provider.provider.networkVersion];
+		const contractDetails = network[Object.keys(network)[0]].contracts[contractName];
+		return { address: contractDetails.address, abi: contractDetails.abi };
+	};
 
-    useEffect(() => {
-        if(provider?.provider){
-            const {address, abi} = getContractDetails(contractName, provider);
-            setContract(new ethers.Contract(address, abi, provider));
-        }
-    }, [provider]);
+	useEffect(() => {
+		if (provider?.provider) {
+			try {
+				const { address, abi } = getContractDetails(contractName, provider);
+				setContract(new ethers.Contract(address, abi, provider));
+			} catch (error) {
+				setContract(undefined);
+				return error.message;
+			}
+		}
+	}, [provider]);
 
-    return contract;
-}
+	return contract;
+};
 
 export default useContract;
