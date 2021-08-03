ded963ac46507bd1ad4f91998dc1c9eb524817a7
Mihirsinh Parmar <mihirsinh.parmar.it@gmail.com>
2021-08-04 03:51:07 +0530

    updated ethers to now connect to default provider if user doesn't have ethereum injected

--- src/ethereum/utils/getEthers.ts
+++ src/ethereum/utils/getEthers.ts
@@ -11,31 +11,22 @@ export interface IEthers {
 
 const getEthers = (): Promise<IEthers> =>
 	new Promise((resolve, reject) => {
-		window.addEventListener('load', async () => {
-			if (window.ethereum) {
-				const provider = new ethers.providers.Web3Provider(window.ethereum);
+		try {
+			window.addEventListener('load', async () => {
 				try {
+					const provider = new ethers.providers.Web3Provider(window.ethereum);
 					await window.ethereum.enable();
 					resolve({ provider, ethers });
 				} catch (error) {
-					console.error(error);
-					reject(error);
-				}
-			} else if (window.web3) {
-				const provider = new ethers.providers.Web3Provider(window.web3);
-				try {
-					await window.web3.enable();
+					const provider = ethers.getDefaultProvider('rinkeby');
+					console.log('No web3 instance injected, using rinkeby web3.');
 					resolve({ provider, ethers });
-				} catch (error) {
-					console.error(error);
-					reject(error);
 				}
-			} else {
-				const provider = new ethers.providers.JsonRpcProvider('http://127.0.0.1:8545');
-				console.log('No web3 instance injected, using local web3.');
-				resolve({ provider, ethers });
-			}
-		});
+			});
+		} catch (error) {
+			alert(error.message);
+			reject(error.message);
+		}
 	});
 
 export default getEthers;
