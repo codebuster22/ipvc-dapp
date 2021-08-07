5c8386fb28fd362b6f2cd3914702cdbfd971f35b
Mihirsinh Parmar <mihirsinh.parmar.it@gmail.com>
2021-08-07 22:25:07 +0530

    removed the magic value from queryEvents.js

--- src/ethereum/utils/queryEvents.js
+++ src/ethereum/utils/queryEvents.js
@@ -1,6 +1,6 @@
 const queryEvents = async (contract, eventName, queryArgs=[]) => {
 	const filter = contract.filters[eventName](...queryArgs);
-	const data = await contract.queryFilter(filter, 8000000);
+	const data = await contract.queryFilter(filter, NEXT_PUBLIC_RINKEBY_START_BLOCK_NUMBER);
 	return data.map((event) => event.args);
 };
 
