a7a69053161b002afab7d06f7a7b5a8cca89d5d3
Angel Lakra <angel.lakra@chainlabs.in>
2021-08-10 22:09:38 +0530

    Updated method for warrior render and login route blocked

--- pages/index.tsx
+++ pages/index.tsx
@@ -9,16 +9,10 @@ const Home = (): JSX.Element => {
 export default Home;
 
 Home.getInitialProps = (ctx) => {
-	if (ctx?.req?.cookies['access_token']) {
-		ctx?.res?.writeHead(301, {
-			Location: '/onboarding',
-		});
-		ctx?.res?.end();
-	} else {
-		ctx?.res?.writeHead(301, {
-			Location: '/login',
-		});
-		ctx?.res?.end();
-	}
+	ctx?.res?.writeHead(301, {
+		Location: '/onboarding',
+	});
+	ctx?.res?.end();
+
 	return {};
 };
