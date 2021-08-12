4e70dd0c9ce178d8a6043521af25d2013aec4cba
Mihirsinh Parmar <mihirsinh.parmar.it@gmail.com>
2021-08-12 20:02:06 +0530

    corrected how we access env variables

--- src/ethereum/utils/queryEvents.js
+++ src/ethereum/utils/queryEvents.js
@@ -1,6 +1,6 @@
 const queryEvents = async (contract, eventName, queryArgs = []) => {
 	const filter = contract.filters[eventName](...queryArgs);
-	const data = await contract.queryFilter(filter, NEXT_PUBLIC_RINKEBY_START_BLOCK_NUMBER);
+	const data = await contract.queryFilter(filter, process.env.NEXT_PUBLIC_RINKEBY_START_BLOCK_NUMBER);
 	return data.map((event) => event.args);
 };
 
