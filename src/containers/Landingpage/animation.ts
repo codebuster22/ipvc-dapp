ac1dd192c3c79078345464573f80f1c41546beed
Angel Lakra <angel.lakra@chainlabs.in>
2021-09-17 17:41:20 +0530

    animation values change

--- src/containers/Landingpage/animation.ts
+++ src/containers/Landingpage/animation.ts
@@ -3,17 +3,18 @@ import { gsap } from 'gsap';
 export const animate = (): void => {
 	gsap.fromTo(
 		'#head1',
-		{ y: '30%', autoAlpha: 0, display: '' },
-		{ y: '0%', autoAlpha: 1, ease: 'easeOutExpo', duration: 0.5 }
+		{ y: '120', autoAlpha: 0, display: '' },
+		{ y: '0', autoAlpha: 1, ease: 'easeOutExpo', duration: 0.5 }
 	);
 	gsap.fromTo(
 		'#head2',
-		{ y: '30%', autoAlpha: 0, display: '' },
-		{ y: '0%', autoAlpha: 1, ease: 'easeOutExpo', duration: 0.5, delay: 0.5 }
+		{ y: '100', autoAlpha: 0, display: '' },
+		{ y: '0', autoAlpha: 1, ease: 'easeOutExpo', duration: 0.5, delay: 0.2 }
 	);
 	gsap.fromTo(
 		'#head3',
-		{ y: '30%', autoAlpha: 0, display: '' },
-		{ y: '0%', autoAlpha: 1, ease: 'easeOutExpo', duration: 0.5, delay: 1 }
+		{ y: '80', autoAlpha: 0, display: '' },
+		{ y: '0', autoAlpha: 1, ease: 'easeOutExpo', duration: 0.5, delay: 0.3 }
 	);
+	gsap.fromTo('#arrow', { y: 0 }, { y: 8, ease: 'easeOutExpo', duration: 0.5, yoyo: true, repeat: -1 });
 };
