09c28c5243491442d31836cfc9a746516a38f1ee
Angel Lakra <angel.lakra@chainlabs.in>
2021-09-17 17:43:38 +0530

    responsive footer

--- src/containers/Landingpage/components/Footer.tsx
+++ src/containers/Landingpage/components/Footer.tsx
@@ -5,95 +5,89 @@ import Twitter from 'svgs/twitter.svg';
 import Discord from 'svgs/discord.svg';
 import Telegram from 'svgs/telegram.svg';
 import { useRouter } from 'next/router';
+import Container from 'components/Container';
 
 const Footer = () => {
 	const router = useRouter();
 	return (
-		<Box backgroundColor="blue-20" width="100%" id="footer">
-			<Box mx="wxxl">
-				<Box between mx="wxxl" alignItems="initial">
-					<Box mr="wxxl" mt="wxl">
-						<Text
-							fontSize="41px"
-							fontFamily="Cinzel Decorative"
-							fontWeight="bold"
-							lineHeight="56px"
-							color="yellow-text"
+		<Box backgroundColor="blue-20" id="footer" px="mxl">
+			<Container>
+				<Box>
+					<Box
+						between
+						alignItems={{ mobS: 'center', tabS: 'flex-start' }}
+						flexDirection={{ mobS: 'column', tabS: 'row' }}
+					>
+						<Box mt={{ mobS: 'mxl', tabS: 'wxl' }}>
+							<Text
+								fontSize="41px"
+								fontFamily="Cinzel Decorative"
+								fontWeight="bold"
+								lineHeight="56px"
+								color="yellow-text"
+							>
+								Warriors
+							</Text>
+							<Text
+								color="white-text"
+								fontSize="14px"
+								lineHeight="14px"
+								fontFamily="El Messiri"
+								fontWeight="bold"
+								textAlign={{ mobS: 'start', tabS: 'end' }}
+								letterSpacing="0.04em"
+								fontStyle="normal"
+							>
+								an NFT project by Chain Labs
+							</Text>
+						</Box>
+						<Box
+							mr={{ mobS: '0', tabS: 'wxxl' }}
+							mt={{ mobS: 'mxl', tabS: 'wxl' }}
+							mb="mxl"
+							center
+							flexDirection={{ mobS: 'row', tabS: 'column' }}
+							maxWidth="100%"
+							flexWrap="wrap"
 						>
-							Warriors
-						</Text>
-						<Text
-							color="#FFFFFF"
-							fontSize="14px"
-							lineHeight="14px"
-							fontFamily="El Messiri"
-							fontWeight="bold"
-							textAlign="end"
-							letterSpacing="0.04em"
-							fontStyle="normal"
-						>
-							an NFT project by Chain Labs
-						</Text>
-					</Box>
-					<Box mr="wxxl" mt="wxl" mb="mxl" px="wxxl">
-						<Text
-							fontSize="28px"
-							color="#D0D0D0"
-							mx="wxxs"
-							fontWeight="bold"
-							fontFamily="El Messiri"
-							lineHeight="44px"
-							letterSpacing="0.045em"
-							mr="wxxl"
-						>
-							About
-						</Text>
-						<Text
-							fontSize="28px"
-							color="#D0D0D0"
-							mx="wxxs"
-							fontWeight="bold"
-							fontFamily="El Messiri"
-							lineHeight="44px"
-							letterSpacing="0.045em"
-							mr="wxxl"
-						>
-							Roadmap
-						</Text>
-						<Text
-							fontSize="28px"
-							color="#D0D0D0"
-							mx="wxxs"
-							fontWeight="bold"
-							fontFamily="El Messiri"
-							lineHeight="44px"
-							letterSpacing="0.045em"
-							mr="wxxl"
-						>
-							FAQ
-						</Text>
-						<Text
-							fontSize="28px"
-							color="#D0D0D0"
-							mx="wxxs"
-							fontWeight="bold"
-							fontFamily="El Messiri"
-							lineHeight="44px"
-							letterSpacing="0.045em"
-							mr="wxxl"
-						>
-							Contact us
-						</Text>
-						<Box display="flex" justifyContent="space-evenly" mx="mxs">
-							<Twitter cursor="pointer" />
-							<Discord onClick={() => router.push('https://discord.gg/fhZzy9Bw')} cursor="pointer" />
-							<Telegram cursor="pointer" />
+							<FooterItem href="#about" content="About" />
+							<FooterItem href="#roadmap" content="Roadmap" />
+							<FooterItem href="#faq" content="FAQs" />
+							<FooterItem href="#contact" content="Contact Us" />
+							<Box
+								display="flex"
+								justifyContent="space-between"
+								mx="mxs"
+								minWidth={{ mobS: '50%', tabS: '100%' }}
+							>
+								<Twitter cursor="pointer" />
+								<Discord onClick={() => router.push('https://discord.gg/fhZzy9Bw')} cursor="pointer" />
+								<Telegram cursor="pointer" />
+							</Box>
 						</Box>
 					</Box>
 				</Box>
-			</Box>
+			</Container>
 		</Box>
 	);
 };
 
 export default Footer;
+
+const FooterItem = ({ href, content, color }: { href: string; content: string; color?: string }) => {
+	return (
+		<a href={href}>
+			<Text
+				as="h3"
+				color={color ?? 'white-text'}
+				mx="wxxs"
+				fontWeight="bold"
+				fontFamily="El Messiri"
+				lineHeight="44px"
+				letterSpacing="0.045em"
+			>
+				{content}
+			</Text>
+		</a>
+	);
+};
